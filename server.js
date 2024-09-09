ea965550db458bdf5fc6917443ed8928b8235205
Samuel J Pope <spope@inrsenterprises.com>
2024-09-09 03:03:29 -0500

    Add body-parser dependency and implement data endpoint

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
 
 const PORT = 3000;
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/data', (req, res) => {
+  const { data } = req.body;
+  fs.appendFileSync('eyeData.txt', JSON.stringify(data) + '\n');
+  res.status(200).send('Data received');
 });
 
 app.listen(PORT, () => {
