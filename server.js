7e35e9da1900eeefeba0b1e5cf4dca3ea84f3b1b
Samuel J Pope <sjpope03@gmail.com>
2024-09-15 20:27:39 -0500

    Refactor webgazer functionality and add session control buttons

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // npm start
-
+// The red dot represents where the system believes you are looking on the screen at that moment.
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
@@ -10,15 +10,48 @@ const PORT = 3000;
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/public/index.html');
+// });
+
+// app.post('/data', (req, res) => {
+//   const { data } = req.body;
+//   fs.appendFileSync('eyeData.txt', JSON.stringify(data) + '\n');
+//   res.status(200).send('Data received');
+// });
+
+
+let sessionData = [];
+let sessionId = 0;
+
+app.post('/start', (req, res) => {
+  sessionId++;
+  sessionData[sessionId] = [];
+  res.send({ message: 'Session started', sessionId: sessionId });
 });
 
 app.post('/data', (req, res) => {
-  const { data } = req.body;
-  fs.appendFileSync('eyeData.txt', JSON.stringify(data) + '\n');
+  const data = req.body.data;
+  if (data && sessionId !== 0) {
+    sessionData[sessionId].push(data);
+  }
   res.status(200).send('Data received');
 });
+
+app.post('/pause', (req, res) => {
+  res.send({ message: 'Session paused', sessionId: sessionId });
+});
+
+app.post('/end', (req, res) => {
+  if (sessionData[sessionId]) {
+    fs.writeFileSync(`session-${sessionId}.txt`, JSON.stringify(sessionData[sessionId]));
+    res.send({ message: 'Session ended and data saved', sessionId: sessionId });
+  } else {
+    res.status(400).send('No session data to save');
+  }
+  sessionId = 0;
+});
+
 
 app.listen(PORT, () => {
   console.log(`I think the server is running on http://localhost:${PORT}`);
